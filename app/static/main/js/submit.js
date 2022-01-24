003ec24a32aaa51784f7e61b4ccbc6d79d987250
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-24 02:40:55 -0800

    Added excel data post feature.

--- app/static/main/js/submit.js
+++ app/static/main/js/submit.js
@@ -4,21 +4,40 @@
 let excel;
 
 
-async function fetchText() {
-    let response = await fetch('/getdata');
+async function fetchText(file) {
+    // console.log("Time begins")
+    // console.time("Rectangle Transmit Time: ")
+
+    let excelData = new FormData();
+    // adding the user selected file to the form
+    excelData.append("mdfExcel", file.files[0]);
+
+    let response = await fetch('/getdata', {method: "POST",body: excelData});
 
     // console.log(response.status); // 200
     // console.log(response.statusText); // OK
-
     if (response.status === 200) {
-        console.log("Time begins")
-        console.time("Rectangle Transmit Time: ")
-        let data = await response.text();
-        excel = JSON.parse(data)
-        console.timeEnd("Rectangle Transmit Time: ")
+         excel = await response.json();
+        // excel = JSON.parse(data)
+        // console.timeEnd("Rectangle Transmit Time for fetch: ")
 
     }
 }
 
-const start = document.getElementById('fetch')
-start.addEventListener('click', fetchText);
+// async function fetchText(file) {
+//     // console.log("Time begins")
+//     // console.time("Rectangle Transmit Time: ")
+//     let response = await fetch('/getdata');
+//
+//     // console.log(response.status); // 200
+//     // console.log(response.statusText); // OK
+//     if (response.status === 200) {
+//          excel = await response.json();
+//         // excel = JSON.parse(data)
+//         // console.timeEnd("Rectangle Transmit Time for fetch: ")
+//
+//     }
+// }
+//
+// const start = document.getElementById('fetch')
+// start.addEventListener('click', fetchText);
